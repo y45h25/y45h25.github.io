86296e4ef74433ee70ddf28f067a0e05c7b8d4c0
YP25 <guggipaul25@gmail.com>
2025-02-27 11:43:50 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+type AppProps = {};
+type AppState = {};
+
+export class App extends Component<AppProps, AppState> {
+
+    render = (): JSX.Element => {
+    
+        return <div>
+          </div>;
+    };
+    
+}
